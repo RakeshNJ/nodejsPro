016156a5d757125ab46e60dee740878a2e092a07
RakeshNJ <96291352+RakeshNJ@users.noreply.github.com>
2022-01-31 14:59:36 +0530

    new setup

---
+++ my-apptest/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Wel-come <code></code> My Name Is Rakesh
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          To React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
